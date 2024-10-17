91be2080a24e142bd44c76ce88f5a587ab1474a1
Antonesei Albert - Octavian <86300499+teyrom12@users.noreply.github.com>
2024-10-17 20:22:41 +0300

    Save-Second Exercise

--- cartridges/app_storefront_custom/cartridge/scripts/helpers/productHelper.js
+++ cartridges/app_storefront_custom/cartridge/scripts/helpers/productHelper.js
@@ -1,3 +1,19 @@
+/**
+* Calculates the discount percentage based on the standard and sale prices.
+* 
+* This function determines the percentage discount given a standard price and a sale price.
+* It returns `null` if any of the following conditions are met:
+* - Either `standardPrice` or `salePrice` is not provided.
+* - Either `standardPrice` or `salePrice` is less than or equal to zero.
+* - `salePrice` is greater than or equal to `standardPrice`.
+* 
+* Otherwise, it calculates the discount percentage using the formula:
+* `((standardPrice - salePrice) / standardPrice) * 100` and returns the result rounded to the nearest integer.
+*
+* @param {number} standardPrice - The original price of the product before any discounts.
+* @param {number} salePrice - The discounted price of the product.
+* @returns {number|null} The discount percentage rounded to the nearest integer, or `null` if the input conditions are not met.
+*/
 'use strict';
 
 function calculateDiscountPercentage(standardPrice, salePrice) {
